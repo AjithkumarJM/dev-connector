77d5f6ab40c0aae695d2c4f8e2e1742a0183bc2b
Ajithkumar JuttunMoorthy <Ajithkumar_juttunmoorthy@comcast.com>
2021-05-13 17:20:55 +0530

    Adds user schema/route

--- config/db.js
+++ config/db.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = async  () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.log(err.message);
-        // Exit process with failure
-        process.exit();
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.log(err.message);
+    // Exit process with failure
+    process.exit();
+  }
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
 const app = express();
 
 // Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -12,7 +15,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-app.get('/', (req, res) => res.send([{name: "ajith"}]));
+app.get('/', (req, res) => res.send([{ name: 'ajith' }]));
 
 const PORT = process.env.PORT || 5000;
 
