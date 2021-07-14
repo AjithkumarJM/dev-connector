32d37d140705a1713e69f7b030c68f67d372eeff
Ajithkumar JuttunMoorthy <Ajithkumar_juttunmoorthy@comcast.com>
2021-07-14 19:46:09 +0530

    Test Commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -60,22 +60,21 @@ router.post(
 
       await user.save();
 
-      res.send('User register successfully');
-      // const payload = {
-      //   user: {
-      //     id: user.id,
-      //   },
-      // };
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      // jwt.sign(
-      //   payload,
-      //   config.get('jwtSecret'),
-      //   { expiresIn: '5 days' },
-      //   (err, token) => {
-      //     if (err) throw err;
-      //     res.json({ token });
-      //   }
-      // );
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
